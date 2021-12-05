e71b8a0307fe80a0b0272f03607f22ccd3546c20
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-12-04 23:28:45 -0800

    Refactor 2021.4

--- advent2021/p4.py
+++ advent2021/p4.py
@@ -1,34 +1,30 @@
 from advent_tools import Puzzle
-from typing import List
 import numpy as np
 
 def solve_a(s: str) -> int:
     batches = s.split("\n\n")
     numbers = [int(num) for num in batches[0].split(",")]
-    boards = [board_from_str(board_str) for board_str in batches[1:]]
-    n, m = len(boards[0]), len(boards[0][0])
-    board_masks = [np.zeros((n, m)) for board in boards]
+    boards = np.stack([board_from_str(board_str) for board_str in batches[1:]])
+    l, n, m = boards.shape
+    board_masks = np.zeros((l, n, m))
 
     have_winner = False
     for num in numbers:
-        for board, board_mask in zip(boards, board_masks):
-            board_mask[np.where(board == num)] = 1
-            if did_board_win(board_mask):
+        for i in range(l):
+            board_masks[i][np.where(boards[i] == num)] = 1
+            if did_board_win(board_masks[i]):
                 have_winner = True
                 break
         if have_winner:
             break
 
-    return get_board_score(board, board_mask, num)
+    return get_board_score(boards[i], board_masks[i], num)
 
 def board_from_str(s: str) -> np.ndarray:
     return np.array([line.split() for line in s.split("\n")], dtype=int)
 
 def did_board_win(board_mask: np.ndarray) -> bool:
-    n, m = board_mask.shape
-    row_wins = (np.all(board_mask[i, :]) for i in range(n))
-    col_wins = (np.all(board_mask[:, j]) for j in range(m))
-    return any(row_wins) or + any(col_wins)
+    return board_mask.all(axis=0).any() or board_mask.all(axis=1).any()
 
 def get_board_score(board: np.ndarray, board_mask: np.ndarray, last_num: int) -> int:
     return board[~board_mask.astype(bool)].sum() * last_num
@@ -36,15 +32,15 @@ def get_board_score(board: np.ndarray, board_mask: np.ndarray, last_num: int) ->
 def solve_b(s: str) -> int:
     batches = s.split("\n\n")
     numbers = [int(num) for num in batches[0].split(",")]
-    boards = [board_from_str(board_str) for board_str in batches[1:]]
-    l, n, m = len(boards), len(boards[0]), len(boards[0][0])
-    board_masks = [np.zeros((n, m)) for board in boards]
+    boards = np.stack([board_from_str(board_str) for board_str in batches[1:]])
+    l, n, m = boards.shape
+    board_masks = np.zeros((l, n, m))
 
     winner_boards = set()
     for num in numbers:
-        for i, (board, board_mask) in enumerate(zip(boards, board_masks)):
-            board_mask[np.where(board == num)] = 1
-            if did_board_win(board_mask):
+        for i in range(l):
+            board_masks[i][np.where(boards[i] == num)] = 1
+            if did_board_win(board_masks[i]):
                 if i in winner_boards:
                     continue
                 winner_boards |= set([i])
@@ -53,7 +49,7 @@ def solve_b(s: str) -> int:
         if len(winner_boards) == l:
             break
 
-    return get_board_score(board, board_mask, num)
+    return get_board_score(boards[i], board_masks[i], num)
 
 
 class Solution:
