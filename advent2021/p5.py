b85b168c3403cff07dc3db8775616d21e7324752
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-12-04 22:57:07 -0800

    Refactor 2021.5

--- advent2021/p5.py
+++ advent2021/p5.py
@@ -1,18 +1,19 @@
-from advent_tools import Puzzle
+import re
 import numpy as np
+from advent_tools import Puzzle
 
 def solve_a(s: str) -> int:
-    lines = [line.split("->") for line in s.split("\n")]
-    lines = np.array([[start.split(","), end.split(",")] for start, end in lines], dtype=int)
-    n, m = lines.max(axis=(0, 1)) + 1
+    matches = (m.groups() for m in re.finditer("(\d+),(\d+) -> (\d+),(\d+)", s))
+    mat = np.array([[(x1, y1), (x2, y2)] for x1, y1, x2, y2 in matches], dtype=int)
+    n, m = mat.max(axis=(0, 1)) + 1
     grid = np.zeros((n, m))
 
-    for pt1, pt2 in lines:
+    for pt1, pt2 in mat:
         if (pt1 == pt2).any():
             xs, ys = make_line(pt1, pt2).T
             grid[xs, ys] += 1
 
-    return (grid > 1).flatten().sum()
+    return (grid > 1).sum()
 
 def make_line(pt1: np.ndarray, pt2: np.ndarray) -> np.ndarray:
     n = np.abs(pt1 - pt2).max()
@@ -20,16 +21,16 @@ def make_line(pt1: np.ndarray, pt2: np.ndarray) -> np.ndarray:
     return np.vstack([pt2 + d * i for i in range(n+1)])
 
 def solve_b(s: str) -> int:
-    lines = [line.split("->") for line in s.split("\n")]
-    lines = np.array([[start.split(","), end.split(",")] for start, end in lines], dtype=int)
-    n, m = lines.max(axis=(0, 1)) + 1
+    matches = (m.groups() for m in re.finditer("(\d+),(\d+) -> (\d+),(\d+)", s))
+    mat = np.array([[(x1, y1), (x2, y2)] for x1, y1, x2, y2 in matches], dtype=int)
+    n, m = mat.max(axis=(0, 1)) + 1
     grid = np.zeros((n, m))
 
-    for pt1, pt2 in lines:
+    for pt1, pt2 in mat:
         xs, ys = make_line(pt1, pt2).T
         grid[xs, ys] += 1
 
-    return (grid > 1).flatten().sum()
+    return (grid > 1).sum()
 
 
 class Solution:
