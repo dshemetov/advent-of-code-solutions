37fb2ad0b19120e8701eb27251773d006b87a0f6
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-12-04 17:38:10 -0800

    Refactor 2020.3

---
+++ advent2020/p3.py
@@ -0,0 +1,44 @@
+from advent_tools import Puzzle
+import numpy as np
+
+def solve_a(s: str) -> int:
+    mat = np.array([list(line) for line in s.split("\n")])
+    n, m = mat.shape
+    mat = np.pad(mat, pad_width=[(0, 0), (0, 3*n)], mode="wrap")
+    return sum(1 if mat[i, 3*i] == "#" else 0 for i in range(n))
+
+def solve_b(s: str) -> int:
+    mat = np.array([list(line) for line in s.split("\n")])
+    n, m = mat.shape
+    mat = np.pad(mat, pad_width=[(0, 0), (0, 7*n)], mode="wrap")
+    trees_slope_11 = sum(1 if mat[i, i] == "#" else 0 for i in range(n))
+    trees_slope_31 = sum(1 if mat[i, 3*i] == "#" else 0 for i in range(n))
+    trees_slope_51 = sum(1 if mat[i, 5*i] == "#" else 0 for i in range(n))
+    trees_slope_71 = sum(1 if mat[i, 7*i] == "#" else 0 for i in range(n))
+    trees_slope_12 = sum(1 if mat[2*i, i] == "#" else 0 for i in range(n // 2))
+    return trees_slope_11 * trees_slope_31 * trees_slope_51 * trees_slope_71 * trees_slope_12
+
+
+class Solution:
+    @property
+    def answer_a(self) -> int:
+        return solve_a(Puzzle(3, 2020).input_data)
+
+    @property
+    def answer_b(self) -> int:
+        return solve_b(Puzzle(3, 2020).input_data)
+
+test_string = """..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"""
+
+assert solve_a(test_string) == 7
+assert solve_b(test_string) == 336
