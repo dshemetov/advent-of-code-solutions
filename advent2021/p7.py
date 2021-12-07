e6aba5684be67b1353c99a27f2bf681fc87d8ca0
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-12-07 00:47:19 -0800

    Refactor 2021.7

--- advent2021/p7.py
+++ advent2021/p7.py
@@ -2,13 +2,14 @@ from advent_tools import Puzzle
 import numpy as np
 
 def solve_a(s: str) -> int:
-    nums = [int(x) for x in s.split(",")]
+    nums = np.array([s.split(",")], dtype=int)
     point = int(np.median(nums))
-    return np.abs(point - np.array(nums)).sum()
+    return np.abs(point - nums).sum()
 
 def solve_b(s: str) -> int:
     nums = np.array([int(x) for x in s.split(",")])
-    return min(point_cost(x, nums) for x in int(np.mean(nums)) + np.array([-2, -1, 0, 1, 2]))
+    options = (np.mean(nums) + np.array([-2, -1, 0, 1, 2])).astype(int)
+    return min(point_cost(x, nums) for x in options)
 
 def point_cost(n: int, nums: np.ndarray) -> int:
     return sum(i*(i+1)//2 for i in np.abs(n - nums))
