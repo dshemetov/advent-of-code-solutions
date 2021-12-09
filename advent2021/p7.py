15e6eda1d86a19fc143f0914fe8ef499d784dabb
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-12-08 20:58:38 -0800

    Refactor 2021.7

--- advent2021/p7.py
+++ advent2021/p7.py
@@ -2,17 +2,17 @@ from advent_tools import Puzzle
 import numpy as np
 
 def solve_a(s: str) -> int:
-    nums = np.array([s.split(",")], dtype=int)
+    nums = np.array(s.split(","), dtype=int)
     point = int(np.median(nums))
     return np.abs(point - nums).sum()
 
 def solve_b(s: str) -> int:
-    nums = np.array([int(x) for x in s.split(",")])
-    options = (np.mean(nums) + np.array([-2, -1, 0, 1, 2])).astype(int)
-    return min(point_cost(x, nums) for x in options)
+    nums = np.array(s.split(","), dtype=int)
+    positions = round(np.mean(nums)) + np.array([-1, 0, 1])
+    return min(position_cost(n, nums) for n in positions)
 
-def point_cost(n: int, nums: np.ndarray) -> int:
-    return sum(i*(i+1)//2 for i in np.abs(n - nums))
+def position_cost(n: int, nums: np.ndarray) -> int:
+    return sum(m*(m+1)//2 for m in np.abs(n - nums))
 
 
 class Solution:
