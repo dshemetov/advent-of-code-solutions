a0e2513571e8929df0be57a2dd3ad821f51fd0a6
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-12-10 01:16:15 -0800

    Refactor 2021.5

--- advent2021/p5.py
+++ advent2021/p5.py
@@ -21,8 +21,11 @@ def make_line(pt1: np.ndarray, pt2: np.ndarray) -> np.ndarray:
     d = (pt1 - pt2) // n
     return np.vstack([pt2 + d * i for i in range(n+1)])
 
-def get_intersection(line1: np.ndarray, line2: np.ndarray) -> Union[np.ndarray, bool]:
-    ...
+def get_intersection(line1: np.ndarray, line2: np.ndarray) -> np.ndarray:
+    (x1, y1), (x2, y2) = line1
+    (v1, w1), (v2, w2) = line2
+    t, s = np.linalg.inv(np.array([[x2-x1, v1-v2], [y2-y1, w1-w2]])) @ np.array([v1 - x1, w1 - y1])
+    return (1-t) * line1[0] + t * line1[1]
 
 def solve_b(s: str) -> int:
     matches = (m.groups() for m in re.finditer("(\d+),(\d+) -> (\d+),(\d+)", s))
