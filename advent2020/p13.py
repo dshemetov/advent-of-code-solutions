6fe1216cff76cfc9092fcc788b7d0a85ec52e3cf
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-12-09 20:51:50 -0800

    Add 2020.13

--- advent2020/p13.py
+++ advent2020/p13.py
@@ -1,46 +1,55 @@
-from advent_tools import Puzzle
-
-from typing import Iterable, Tuple, List
-
-def parse_input_a(s: str) -> Tuple[int, List]:
-    depart_time, bus_schedule = s.split("\n")
-    depart_time = int(depart_time)
-    bus_schedule = filter(lambda x: x != "x", bus_schedule.split(","))
-    return depart_time, bus_schedule
-
-def parse_input_b(s: str) -> List[Tuple[int, int]]:
-    _, bus_schedule = s.split("\n")
-    bus_schedule = enumerate(bus_schedule.split(","))
-    bus_schedule = filter(lambda x: x[1] != "x", bus_schedule)
-    bus_schedule = list((int(e[1]), e[0]) for e in bus_schedule)
-    return bus_schedule
-
-def get_minutes_until_next_bus(i: int, n: int) -> int:
-    if i % n == 0:
-        return 0
-    else:
-        return n - i % n
-
-def get_next_bus_time(i: int, n: int) -> int:
-    return i + get_minutes_until_next_bus(i, n)
+from advent_tools import Puzzle, get_bezout_coefficients, get_gcd
+from itertools import product
+from typing import Tuple, List
 
 def solve_a(s: str) -> int:
-    depart_time, bus_schedule = parse_input_a(s)
+    depart_time, bus_schedule = s.split("\n")
+    depart_time, bus_schedule = int(depart_time), filter(lambda x: x != "x", bus_schedule.split(","))
     bus_id, next_bus_time = min([(int(bus_id), get_next_bus_time(depart_time, int(bus_id))) for bus_id in bus_schedule], key=lambda x: x[1])
     return bus_id * (next_bus_time - depart_time)
 
-def find_timestamp(bus_schedule: Iterable[Tuple[int, int]]) -> int:
-    largest_id, largest_id_wait_time = max(bus_schedule, key=lambda x: x[0])
-    t = largest_id - largest_id_wait_time
-    while True:
-        if all(get_minutes_until_next_bus(t, bus_id) == wait_time for bus_id, wait_time in bus_schedule):
-            break
-        t += largest_id
-    return t
+def get_next_bus_time(time: int, period: int) -> int:
+    return time + get_minutes_until_next_bus(time, period)
+
+def get_minutes_until_next_bus(time: int, period: int) -> int:
+    if time % period == 0:
+        return 0
+    else:
+        return period - time % period
 
 def solve_b(s: str) -> int:
-    bus_schedule = parse_input_b(s)
-    return find_timestamp(bus_schedule)
+    _, bus_schedule = s.split("\n")
+    bus_schedule: List[Tuple[int, int]] = list((-offset, int(bus_id)) for offset, bus_id in enumerate(bus_schedule.split(",")) if bus_id != "x")
+    return solve_modular_congruence_equations(bus_schedule)
+
+def solve_modular_congruence_equations(residue_classes: List[Tuple[int, int]]) -> int:
+    """Solve an arbitrary system of modular equations.
+
+    Elements in residue_classes are assumed to be pairs (a_i, b_i) and the system of equations to solve is:
+        x = a_i mod b_i,          i = 1, 2, ...
+    """
+    if not all(get_gcd(a, b) == 1 for a, b in product((m for _, m in residue_classes), repeat=2) if a != b):
+        raise ValueError("The moduli in the system are not all pair-wise relatively prime.")
+
+    residue_classes_ = residue_classes.copy()
+    while len(residue_classes_) > 1:
+        bus1, bus2 = residue_classes_.pop(), residue_classes_.pop()
+        a, b = solve_modular_congruence_equation_pair(bus1, bus2)
+        residue_classes_ += [(a, b)]
+    return a
+
+def solve_modular_congruence_equation_pair(b1: Tuple[int, int], b2: Tuple[int, int]) -> Tuple[int, int]:
+    """Solve a system of two modular equations.
+
+    Solves the system
+        x = a1 mod n1
+        x = a2 mod n2
+    where a1, n1 = b1 and a2, n2 = b2.
+    """
+    (a1, n1), (a2, n2) = b1, b2
+    s, t = get_bezout_coefficients(n1, n2)
+    x = a2 * s * n1 + a1 * t * n2
+    return (x % (n1 * n2), n1 * n2)
 
 
 class Solution:
--- advent2020/test_p13.py
+++ advent2020/test_p13.py
@@ -1,5 +1,5 @@
 import pytest
-from .p13 import parse_input_a, parse_input_b, get_next_bus_time, find_timestamp
+from .p13 import solve_a, solve_b, get_next_bus_time, solve_modular_congruence_equation_pair
 
 test_strings = [
     """939\n7,13,x,x,59,x,31,19""",
@@ -10,25 +10,22 @@ test_strings = [
     """1\n1789,37,47,1889"""
 ]
 
-class Test13a:
-    def test_get_next_bus_time(self):
-        assert get_next_bus_time(14, 7) == 14
-        assert get_next_bus_time(15, 7) == 21
-        assert get_next_bus_time(36, 11) == 44
+def test_get_next_bus_time():
+    assert get_next_bus_time(14, 7) == 14
+    assert get_next_bus_time(15, 7) == 21
+    assert get_next_bus_time(36, 11) == 44
 
-    def test_problem(self):
-        depart_time, bus_schedule = parse_input_a(test_strings[0])
-        bus_id, next_bus_time = min([(int(bus_id), get_next_bus_time(depart_time, int(bus_id))) for bus_id in bus_schedule], key=lambda x: x[1])
-        assert bus_id * (next_bus_time - depart_time) == 295
+def test_solve_a():
+    assert solve_a(test_strings[0]) == 295
 
-class Test13b:
-    def test_parse_input_b(self):
-        assert list(parse_input_b(test_strings[0])) == [(7, 0), (13, 1), (59, 4), (31, 6), (19, 7)]
+def test_solve_modular_congruence_equation_pair():
+    assert solve_modular_congruence_equation_pair((2, 5), (3, 7)) == (17, 35)
+    assert solve_modular_congruence_equation_pair((2, 5), (3, 7)) == (17, 35)
 
-    def test_find_timestamp(self):
-        assert find_timestamp(parse_input_b(test_strings[0])) == 1068781
-        assert find_timestamp(parse_input_b(test_strings[1])) == 3417
-        assert find_timestamp(parse_input_b(test_strings[2])) == 754018
-        assert find_timestamp(parse_input_b(test_strings[3])) == 779210
-        assert find_timestamp(parse_input_b(test_strings[4])) == 1261476
-        assert find_timestamp(parse_input_b(test_strings[5])) == 1202161486
+def test_solve_b():
+    assert solve_b(test_strings[0]) == 1068781
+    assert solve_b(test_strings[1]) == 3417
+    assert solve_b(test_strings[2]) == 754018
+    assert solve_b(test_strings[3]) == 779210
+    assert solve_b(test_strings[4]) == 1261476
+    assert solve_b(test_strings[5]) == 1202161486
