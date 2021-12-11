178539160bfecd33e780df5579c24be70c006a02
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-12-10 22:57:55 -0800

    Add 2021.11

---
+++ advent2021/p11.py
@@ -0,0 +1,64 @@
+from advent_tools import Puzzle
+from typing import List, Tuple
+import numpy as np
+from itertools import product
+
+directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+def solve_a(s: str) -> int:
+    mat = parse_input(s)
+    _, flashes = run_octopus_steps(mat, 100)
+    return flashes
+
+def parse_input(s: str) -> np.ndarray:
+    return np.array([list(line) for line in s.split("\n")], dtype=int)
+
+def get_neighbor_values(i: int, j: int, mat: List[List[int]]) -> List[int]:
+    return (mat[i][j] for i, j in get_valid_neighbor_ixs(i, j, mat))
+
+def get_valid_neighbor_ixs(i: int, j: int, mat: List[List[int]]) -> List[Tuple[int, int]]:
+    return ((i + di, j + dj) for di, dj in directions if 0 <= i + di < len(mat) and 0 <= j + dj < len(mat[0]))
+
+def run_octopus_step(mat: np.ndarray) -> Tuple[np.ndarray, int]:
+    mat = mat.copy()
+    mat += 1
+    n, m = mat.shape
+    already_flashed = set()
+    ixs_to_check = set((i, j) for i, j in product(range(n), range(m)) if mat[i, j] > 9)
+    while len(ixs_to_check) > 0:
+        i, j = ixs_to_check.pop()
+        if mat[i, j] > 9 and (i, j) not in already_flashed:
+            already_flashed |= {(i, j)}
+            for i_, j_ in get_valid_neighbor_ixs(i, j, mat):
+                mat[i_, j_] += 1
+                ixs_to_check |= {(i_, j_)}
+    mat[np.where(mat > 9)] = 0
+    return mat, len(already_flashed)
+
+def run_octopus_steps(mat: np.ndarray, n: int) -> Tuple[np.ndarray, int]:
+    flash_counter = 0
+    mat_ = mat.copy()
+    for _ in range(n):
+        mat_, flashes = run_octopus_step(mat_)
+        flash_counter += flashes
+    return mat_, flash_counter
+
+def solve_b(s: str) -> int:
+    mat = parse_input(s)
+    i = 1
+    while True:
+        mat, _ = run_octopus_step(mat)
+        if (mat == 0).all():
+            break
+        i += 1
+    return i
+
+
+class Solution:
+    @property
+    def answer_a(self) -> int:
+        return solve_a(Puzzle(11, 2021).input_data)
+
+    @property
+    def answer_b(self) -> int:
+        return solve_b(Puzzle(11, 2021).input_data)
