629f7ffd25df6ede9483b7183c68915d26f4ee66
Dmitry Shemetov <dshemetov@ucdavis.edu>
2022-08-20 05:27:12 -0700

    Cleanup runner.py

--- runner.py
+++ runner.py
@@ -2,6 +2,7 @@
 
 Usage:
     runner.py problem <year.day.part> [-c | --clear-cache]
+    runner.py day <year.day> [-c | --clear-cache]
     runner.py year <year> [-c | --clear-cache]
     runner.py -h | --help
     runner.py --version
@@ -11,6 +12,7 @@ Options:
     --version            Show version.
     -c --clear-cache     Clear cached solutions, if already computed.
 """
+from typing import Tuple
 from docopt import docopt
 from importlib import import_module
 import time
@@ -44,30 +46,46 @@ def is_cached(year: int, day: int, part: str) -> int:
     except KeyError:
         return False
 
-def get_day_answer(year: int, day: int, part: str, clear_cache: bool, verbose: bool = True) -> int:
-    if clear_cache and is_cached(year, day, part):
-        if verbose:
-            print(f"Recalculating answer to {year}.{day}.{part}...")
-        result = get_answer.call_and_shelve(year, day, part).clear()
+def timed(func):
+    def new_func(*args, **kwargs):
         t = time.perf_counter()
+        out = func(*args, **kwargs)
+        time_elapsed = time.perf_counter() - t
+        return out, time_elapsed
+    return new_func
+
+@timed
+def get_answer_cache(year: int, day: int, part: str, clear_cache: bool, verbose: bool = False) -> int:
+    if clear_cache and is_cached(year, day, part):
+        result = get_answer.call_and_shelve(year, day, part).clear()
         ans = get_answer(year, day, part)
-        time_taken = time.perf_counter() - t
         if result != ans and verbose:
             print(f"Warning, new result differs from cached for {year}.{day}.{part}...")
-    elif is_cached(year, day, part):
-        if verbose:
-            print(f"Looking up cached answer to {year}.{day}.{part}...")
-        ans = get_answer(year, day, part)
-        time_taken = 0
     else:
-        if verbose:
-            print(f"Calculating uncached answer to {year}.{day}.{part}...")
-        t = time.perf_counter()
         ans = get_answer(year, day, part)
-        time_taken = time.perf_counter() - t
+    return ans
 
+def get_part_solution(year: int, day: int, part: str, clear_cache: bool, verbose: bool = False) -> Tuple[int, float]:
+    try:
+        ans, time_taken = get_answer_cache(year, day, part, clear_cache, verbose)
+        print(f"Day {day:>2}. Solution {part}: {ans:>20} (elapsed time {time_taken:>5.3f}).")
+    except ModuleNotFoundError:
+        ans, time_taken = 0, 0
     return ans, time_taken
 
+def get_day_solutions(year: int, day: int, clear_cache: bool, verbose: bool = False) -> Tuple[Tuple[int, float], Tuple[int, float]]:
+    ans1, time_taken1 = get_part_solution(year, day, "a", clear_cache, verbose)
+    ans2, time_taken2 = get_part_solution(year, day, "b", clear_cache, verbose)
+    return (ans1, time_taken1), (ans2, time_taken2)
+
+def get_year_solutions(year: int, clear_cache: bool):
+    print(f"{year} Solutions:")
+
+    total_time_taken = 0
+    for day in range(1, 26):
+        (_, time_taken1), (_, time_taken2) = get_day_solutions(year, day, clear_cache)
+        total_time_taken += time_taken1 + time_taken2
+    print(f"Total time taken: {total_time_taken}.")
 
 if __name__ == "__main__":
     args = docopt(__doc__, version='Advent of Code Solution Runner v0.1.0')
@@ -75,22 +93,13 @@ if __name__ == "__main__":
     if args.get("problem"):
         year, day, part = args["<year.day.part>"].split(".")
         year, day = int(year), int(day)
-        ans, time_taken = get_day_answer(year, day, part, args["--clear-cache"])
-        print(ans)
-        print(f"(elapsed time: {time_taken:.3f} seconds)")
+        get_part_solution(year, day, part, args["--clear-cache"])
+
+    if args.get("day"):
+        year, day = args["<year.day>"].split(".")
+        year, day = int(year), int(day)
+        get_day_solutions(year, day, args["--clear-cache"])
 
     if args.get("year"):
         year = int(args["<year>"])
-        print(f"{year} Solutions:")
-        total_time_taken = 0
-        for day in range(1, 26):
-            try:
-                ans1, time_taken1 = get_day_answer(year, day, "a", args["--clear-cache"], verbose=False)
-                ans2, time_taken2 = get_day_answer(year, day, "b", args["--clear-cache"], verbose=False)
-            except ModuleNotFoundError:
-                ans1, time_taken1 = 0, 0
-                ans2, time_taken2 = 0, 0
-            
-            total_time_taken = total_time_taken + time_taken1 + time_taken2
-            print(f"Day {day : >2}. Solution A: {ans1 : >16}, elapsed time {time_taken1 : .3f}. Solution B: {ans2 : >16}, elapsed time {time_taken2 : .3f}.")
-        print(f"Total time taken: {total_time_taken}.")
+        get_year_solutions(year, args["--clear-cache"])
