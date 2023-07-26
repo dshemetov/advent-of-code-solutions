d704e7863e22392251714b943cec7ac42002cff2
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-07-26 01:00:46 -0700

    feat: better 2021.18 solution

--- advent/advent2021/p18.py
+++ advent/advent2021/p18.py
@@ -1,197 +1,120 @@
 """Snailfish
 https://adventofcode.com/2021/day/18
+
+This is mostly an elegant solution found on Reddit. It was so much cleaner than
+what we had come up with, that I decided to keep it.
 """
-from copy import deepcopy
-from math import ceil, floor
-
-import numpy as np
+import functools
+import re
 
 
-def solve_a(s: str) -> int:
-    if not s:
-        return 0
-    root_trees = parse_input(s)
-    end_tree = root_trees[0]
-    for root_tree in root_trees[1:]:
-        while end_tree.reduce():
-            continue
-        while root_tree.reduce():
-            continue
-        end_tree = end_tree + root_tree
+def solve_a(s: str):
+    """
+    Examples:
+    >>> solve_a(test_string)
+    4140
+    """
+    numbers = [tokenize(line.strip()) for line in s.strip("\n").split("\n")]
 
-    while end_tree.reduce():
+    total = functools.reduce(
+        lambda left, right: reduct(["["] + left + right + ["]"]), numbers
+    )
+    return magnitude(total)
+
+
+def tokenize(expr: str) -> list[int | str]:
+    """
+    Examples:
+    >>> s = tokenize("[[[[[9,8],1],2],3],4]")
+    >>> s
+    ['[', '[', '[', '[', '[', 9, 8, ']', 1, ']', 2, ']', 3, ']', 4, ']']
+    """
+    return [
+        int(token) if "0" <= token[0] <= "9" else token
+        for token in re.findall(r"\[|\]|\d+", expr)
+    ]
+
+
+def reduct(tokens: list[int | str]) -> list[int | str]:
+    while explode(tokens) or split(tokens):
         continue
-
-    return end_tree.magnitude()
-
-
-class SnailfishTree:
-    def __init__(self, val: int | None = None, depth: int = 0):
-        self.left, self.right, self.parent = None, None, None
-        self.val, self.depth = val, depth
-
-    def parse(
-        self,
-        input: list | int,
-        depth: int = 0,
-        parent: "SnailfishTree | None" = None,
-    ) -> None:
-        self.depth = depth
-        self.parent = parent
-        if isinstance(input, list):
-            self.left = SnailfishTree()
-            self.left.parse(input[0], depth + 1, self)
-            self.right = SnailfishTree()
-            self.right.parse(input[1], depth + 1, self)
-        else:
-            self.val = input
-
-    def increment_depth(self) -> None:
-        self.depth += 1
-        if self.left is not None:
-            self.left.increment_depth()
-        if self.right is not None:
-            self.right.increment_depth()
-
-    def flatten_to_list(self, to_add: list["SnailfishTree"]):
-        if self.val is not None:
-            to_add.append(self)
-        else:
-            self.left.flatten_to_list(to_add)
-            self.right.flatten_to_list(to_add)
-
-    def get_right(self, list_of_nodes: list["SnailfishTree"]) -> "SnailfishTree":
-        assert self.val is not None
-        ind = list_of_nodes.index(self)
-        return list_of_nodes[ind + 1] if ind + 1 < len(list_of_nodes) else None
-
-    def get_left(self, list_of_nodes: list["SnailfishTree"]) -> "SnailfishTree":
-        assert self.val is not None
-        ind = list_of_nodes.index(self)
-        return list_of_nodes[ind - 1] if ind > 0 else None
-
-    def needs_explosion(self) -> bool:
-        return self.depth > 4
-
-    def needs_split(self) -> bool:
-        return self.val is not None and self.val >= 10
-
-    def explode(self, list_of_nodes: list["SnailfishTree"]) -> None:
-        l, r = self.left.val, self.right.val
-
-        right_neighbor = self.right.get_right(list_of_nodes)
-        if right_neighbor is not None:
-            right_neighbor.val += r
-
-        left_neighbor = self.left.get_left(list_of_nodes)
-        if left_neighbor is not None:
-            left_neighbor.val += l
-
-        self.val = 0
-        self.left = None
-        self.right = None
-
-    def split(self):
-        assert self.val is not None
-        assert self.right is None
-        assert self.left is None
-        cur_val = self.val
-        self.val = None
-        self.left = SnailfishTree(floor(cur_val / 2))
-        self.right = SnailfishTree(ceil(cur_val / 2))
-        self.left.parent = self
-        self.right.parent = self
-        self.left.depth = self.depth + 1
-        self.right.depth = self.depth + 1
-        # fin
-
-    def magnitude(self) -> int:
-        # The magnitude of a pair is 3 times the magnitude of its left element plus
-        # 2 times the magnitude of its right element. The magnitude of a regular
-        # number is just that number.
-        if self.val is not None:
-            return self.val
-        else:
-            return 3 * self.left.magnitude() + 2 * self.right.magnitude()
-
-    def __repr__(self) -> str:
-        if self.val is not None:
-            return f"{self.val}"
-        else:
-            return f"[{self.left},{self.right}]"
-
-    def __add__(self, other: "SnailfishTree") -> "SnailfishTree":
-        assert self.depth == other.depth == 0
-        parent = SnailfishTree()
-        parent.depth = self.depth
-        parent.left = self
-        parent.right = other
-        parent.left.increment_depth()
-        parent.right.increment_depth()
-        self.parent = other.parent = parent
-        return parent
-
-    def to_list(self) -> list[int]:
-        if self.parent is None:
-            return self.left.to_list() + self.right.to_list()
-        elif self.val is None:
-            return [self.left.to_list() + self.right.to_list()]
-        else:
-            return [self.val]
-
-    def get_max_depth(self, maxd: int) -> int:
-        if self.val is not None:
-            return max(maxd, self.depth)
-        else:
-            return max(self.left.get_max_depth(maxd), self.right.get_max_depth(maxd))
-
-    def reduce(self) -> bool:
-        flattened_list = []
-        self.flatten_to_list(flattened_list)
-        # max_depth = self.get_max_depth(-np.inf)
-        for leaf in flattened_list:
-            if leaf.needs_explosion():
-                try:
-                    leaf.parent.explode(flattened_list)
-                except:
-                    print(f"Leaf: {leaf}")
-                    print(f"leaf.parent: {leaf.parent}")
-                    raise Exception("Your doom!")
-                return True
-
-        for leaf in flattened_list:
-            if leaf.needs_split():
-                leaf.split()
-                return True
-
-        return False
+    return tokens
 
 
-def parse_input(s: str):
-    data_lines = s.split("\n")
-    root_trees = []
-    for data_line in data_lines:
-        arr = eval(data_line.strip())
-        root_tree = SnailfishTree()
-        root_tree.parse(arr)
-        root_trees.append(root_tree)
-    return root_trees
+def explode(tokens: list[int | str]) -> bool:
+    """Returns True if an explosion occurred, False otherwise. Side effect: modifies tokens.
+
+    Examples:
+    >>> s = tokenize("[[[[[9,8],1],2],3],4]")
+    >>> explode(s)
+    True
+    >>> s
+    ['[', '[', '[', '[', 0, 9, ']', 2, ']', 3, ']', 4, ']']
+    """
+    depth = 0
+    for index in range(len(tokens) - 4):
+        if depth >= 4 and tokens[index] == "[" and tokens[index + 3] == "]":
+            for left in range(index - 1, -1, -1):
+                if isinstance(tokens[left], int):
+                    tokens[left] += tokens[index + 1]
+                    break
+            for right in range(index + 4, len(tokens)):
+                if isinstance(tokens[right], int):
+                    tokens[right] += tokens[index + 2]
+                    break
+            # Note: this contracts the length of the list by 3 elements
+            tokens[index : index + 4] = [0]
+            return True
+        elif tokens[index] == "[":
+            depth += 1
+        elif tokens[index] == "]":
+            depth -= 1
+    return False
 
 
-def solve_b(s: str) -> int:
-    if not s:
-        return 0
-    root_trees = parse_input(s)
-    max_mag = -np.inf
-    for i in range(len(root_trees)):
-        for j in range(len(root_trees)):
-            if i != j:
-                tree1 = deepcopy(root_trees[i])
-                tree2 = deepcopy(root_trees[j])
-                out = tree1 + tree2
-                while out.reduce():
-                    pass
+def split(tokens):
+    for index in range(len(tokens)):
+        if isinstance(tokens[index], int) and tokens[index] >= 10:
+            tokens[index : index + 1] = [
+                "[",
+                (tokens[index] + 0) // 2,
+                (tokens[index] + 1) // 2,
+                "]",
+            ]
+            return True
+    return False
 
-                max_mag = max(max_mag, out.magnitude())
 
-    return max_mag
+def magnitude(tokens):
+    stack = []
+    for token in tokens:
+        if isinstance(token, int):
+            stack.append(token)
+        elif token == "]":
+            stack[-2:] = [3 * stack[-2] + 2 * stack[-1]]
+    return stack[-1]
+
+
+def solve_b(s: str):
+    numbers = [tokenize(line.strip()) for line in s.strip("\n").split("\n")]
+
+    largest = 0
+    for left in numbers:
+        for right in numbers:
+            total = reduct(["["] + left + right + ["]"])
+            largest = max(largest, magnitude(total))
+    return largest
+
+
+test_string = """
+[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+"""
