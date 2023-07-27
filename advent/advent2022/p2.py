933a7be40c7da949f51b291fbd91834ef678ec74
Dmitry Shemetov <dshemetov@ucdavis.edu>
2023-07-26 21:17:51 -0700

    refactor(2022.2): use match, case

--- advent/advent2022/p2.py
+++ advent/advent2022/p2.py
@@ -12,28 +12,26 @@ def solve_a(s: str) -> int:
     s = s.strip("\n")
     total = 0
     for line in s.splitlines():
-        a, b = line.split()
-        if a == "A":
-            if b == "X":
+        match line.split():
+            case "A", "X":
                 total += 3 + 1
-            elif b == "Y":
+            case "A", "Y":
                 total += 6 + 2
-            elif b == "Z":
+            case "A", "Z":
                 total += 0 + 3
-        elif a == "B":
-            if b == "X":
+            case "B", "X":
                 total += 0 + 1
-            elif b == "Y":
+            case "B", "Y":
                 total += 3 + 2
-            elif b == "Z":
+            case "B", "Z":
                 total += 6 + 3
-        elif a == "C":
-            if b == "X":
+            case "C", "X":
                 total += 6 + 1
-            elif b == "Y":
+            case "C", "Y":
                 total += 0 + 2
-            elif b == "Z":
+            case "C", "Z":
                 total += 3 + 3
+
     return total
 
 
@@ -46,28 +44,24 @@ def solve_b(s: str) -> int:
     s = s.strip("\n")
     total = 0
     for line in s.splitlines():
-        a, b = line.split()
-
-        if b == "X":
-            if a == "A":
+        match line.split():
+            case "A", "X":
                 total += 0 + 3
-            elif a == "B":
-                total += 0 + 1
-            elif a == "C":
-                total += 0 + 2
-        elif b == "Y":
-            if a == "A":
+            case "A", "Y":
                 total += 3 + 1
-            elif a == "B":
-                total += 3 + 2
-            elif a == "C":
-                total += 3 + 3
-        elif b == "Z":
-            if a == "A":
+            case "A", "Z":
                 total += 6 + 2
-            elif a == "B":
+            case "B", "X":
+                total += 0 + 1
+            case "B", "Y":
+                total += 3 + 2
+            case "B", "Z":
                 total += 6 + 3
-            elif a == "C":
+            case "C", "X":
+                total += 0 + 2
+            case "C", "Y":
+                total += 3 + 3
+            case "C", "Z":
                 total += 6 + 1
 
     return total
